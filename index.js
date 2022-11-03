4190dbfb843de20b7e484b29ee4ac92182a5ed9d
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-11-04 00:52:35 +0600

    products api

--- index.js
+++ index.js
@@ -20,6 +20,34 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
+    const database = client.db("emaJohn");
+    const productsCollection = database.collection("products");
+
+    // get all products
+    app.get("/products", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log(page, size);
+      const query = {};
+      const options = {};
+      const cursor = productsCollection.find(query, options);
+      const products = await cursor
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      const count = await productsCollection.estimatedDocumentCount();
+      res.send({ count, products });
+    });
+
+    app.post("/productsByIds", async (req, res) => {
+      const ids = req.body;
+      const objectIds = ids.map(id => ObjectId(id));
+      const query = { _id: { $in: objectIds } };
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+
+      res.send(products);
+    });
   } catch (error) {
     console.error(error);
   } finally {
